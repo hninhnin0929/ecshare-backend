4d3e94e2de12de7316289506dd5545169ff5a777
hnin <hnin0929@ecplaza.net>
2024-03-29 11:35:39 +0900

    timestamp at file save

--- models/fileModel.js
+++ models/fileModel.js
@@ -1,15 +1,29 @@
 // models/fileModel.js
 const mongoose = require('mongoose');
 
-const fileSchema = new mongoose.Schema({
-    filename: String,
-    size: Number,
-    url: String,
-    user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
+const fileSchema = new mongoose.Schema(
+    {
+        filename: {
+            type: String,
+            required: [true, "File name should not be blank"],
+        },
+        size: {
+            type: Number,
+            required: [true, "File size should not be blank"]
+        },
+        url: {
+            type: String,
+            required: [true, "File URL is needed"]
+        },
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User",
+        }
     },
-});
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model('File', fileSchema);
